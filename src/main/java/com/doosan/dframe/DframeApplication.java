daf7ce55a8393831b68ffef34d7fd1b64c65b047
cusonar <cu.sonar@gmail.com>
2026-05-07 23:04:41 +0900

    feat: initialize core project structure, entity domain models, and JSON-based data loading system

---
+++ src/main/java/com/doosan/dframe/DframeApplication.java
@@ -0,0 +1,13 @@
+package com.doosan.dframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DframeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DframeApplication.class, args);
+    }
+
+}
